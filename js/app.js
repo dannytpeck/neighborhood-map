730ff5a1214211a222690ab13c5e15286a6791f7
Danny Peck <dannytpeck@gmail.com>
2017-11-10 11:27:39 -0800

    Add knockout and set locations as observables

--- js/app.js
+++ js/app.js
@@ -1,10 +1,49 @@
+// These are the pizza locations the app initializes with
+var initialLocations = [
+  {title: 'Big Mario\'s Pizza', location: {lat: 47.6601293, lng: -122.3655865}, yelpId: 'big-marios-pizza-seattle-6'},
+  {title: 'Frēlard Pizza Company', location: {lat: 47.6537991, lng: -122.3597635}, yelpId: 'frelard-pizza-company-seattle'},
+  {title: 'Chinapie', location: {lat: 47.6523581, lng: -122.356624}, yelpId: 'chinapie-seattle-3'},
+  {title: 'Via Tribunali', location: {lat: 47.6588171, lng: -122.3500846}, yelpId: 'via-tribunali-seattle-6'},
+  {title: 'Ballard Pizza Company', location: {lat: 47.6612894, lng: -122.3306091}, yelpId: 'ballard-pizza-company-seattle'},
+  {title: 'Pagliacci Pizza', location: {lat: 47.6539202, lng: -122.3437461}, yelpId: 'pagliacci-pizza-seattle-3'},
+  {title: 'Zeeks Pizza', location: {lat: 47.648321, lng: -122.3559744}, yelpId: 'zeeks-pizza-seattle'},
+  {title: 'Ballroom', location: {lat: 47.6516998, lng: -122.3532554}, yelpId: 'the-ballroom-seattle'}
+];
+
+var Joint = function(data) {
+  this.title = ko.observable(data.title);
+  this.location = ko.observable(data.location);
+  this.yelpId = ko.observable(data.yelpId);
+};
+
+var ViewModel = function() {
+  var self = this;
+
+  this.locations = ko.observableArray([]);
+
+  this.filterLocations = function() {
+    var searchText = document.querySelector('#filter-text').value.toLowerCase();
+
+    // Clear locations observable array
+    self.locations.removeAll();
+
+    // Cycle through initial Locations and only add ones that match the search Text
+    initialLocations.forEach(function(location) {
+      if (location.title.toLowerCase().includes(searchText)) {
+        self.locations.push(new Joint(location))
+      }
+    });
+  };
+
+  initialLocations.forEach(function(location) {
+    self.locations.push(new Joint(location));
+  });
+};
+
 var map;
 
 // Create a new blank array for all the listing markers.
 var markers = [];
-
-// This global polygon variable is to ensure only ONE polygon is rendered.
-var polygon = null;
 
 function initMap() {
   // Constructor creates a new map - only center and zoom are required.
@@ -14,36 +53,13 @@ function initMap() {
     mapTypeControl: false
   });
 
-  // https://api.yelp.com/oauth2/token
-  // client_id: nkAiQEV6rXonD1gIUTXhdQ
-  // client_secret: 6dtZFXhBVhVrJ1utJZwwz5KZ1ytLeRy3qHhj4OsUV91sX9O5mxULUAcJpBovUUC5
-  // grant_type: client_credentials
-  // => { access_token: blahblah }
-
-  // https://api.yelp.com/v3/businesses/north-india-restaurant-san-francisco
-  // Bearer Mb84WblpHpm47Sa2Vwh5fqS6GlrYz69ZTKEhxrtOuQRdNwLGFgRen1Fa4pq8hS5tr9GKTpLSwns9ONYqzpqnnxNAmZ4MIhlbG-gBA7EI-j50FmzKTJP08timIr4EWnYx
-  // => { image_url, display_phone, rating }
-
-  // These are the pizza locations that will be shown to the user.
-  // Normally we'd have these in a database instead.
-  var locations = [
-    {title: 'Big Mario\'s Pizza', location: {lat: 47.6601293, lng: -122.3655865}, yelpId: 'big-marios-pizza-seattle-6'},
-    {title: 'Frēlard Pizza Company', location: {lat: 47.6537991, lng: -122.3597635}, yelpId: 'frelard-pizza-company-seattle'},
-    {title: 'Chinapie', location: {lat: 47.6523581, lng: -122.356624}, yelpId: 'chinapie-seattle-3'},
-    {title: 'Via Tribunali', location: {lat: 47.6588171, lng: -122.3500846}, yelpId: 'via-tribunali-seattle-6'},
-    {title: 'Ballard Pizza Company', location: {lat: 47.6612894, lng: -122.3306091}, yelpId: 'ballard-pizza-company-seattle'},
-    {title: 'Pagliacci Pizza', location: {lat: 47.6539202, lng: -122.3437461}, yelpId: 'pagliacci-pizza-seattle-3'},
-    {title: 'Zeeks Pizza', location: {lat: 47.648321, lng: -122.3559744}, yelpId: 'zeeks-pizza-seattle'},
-    {title: 'Ballroom', location: {lat: 47.6516998, lng: -122.3532554}, yelpId: 'the-ballroom-seattle'}
-  ];
-
   var largeInfowindow = new google.maps.InfoWindow();
 
   // The following group uses the location array to create an array of markers on initialize.
-  for (var i = 0; i < locations.length; i++) {
+  for (var i = 0; i < initialLocations.length; i++) {
     // Get the position from the location array.
-    var position = locations[i].location;
-    var title = locations[i].title;
+    var position = initialLocations[i].location;
+    var title = initialLocations[i].title;
     // Create a marker per location, and put into markers array.
     var marker = new google.maps.Marker({
       position: position,
@@ -59,12 +75,14 @@ function initMap() {
     });
   }
 
-  document.getElementById('show-listings').addEventListener('click', showListings);
-  document.getElementById('hide-listings').addEventListener('click', hideListings);
-
-  document.getElementById('zoom-to-area').addEventListener('click', function() {
-    zoomToArea();
-  });
+  // Show all markers by default
+  var bounds = new google.maps.LatLngBounds();
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+    bounds.extend(markers[i].position);
+  }
+  // Extend the boundaries of the map for each marker and display the marker
+  map.fitBounds(bounds);
 
 }
 
@@ -83,6 +101,7 @@ function populateInfoWindow(marker, infowindow) {
     });
     var streetViewService = new google.maps.StreetViewService();
     var radius = 50;
+
     // In case the status is OK, which means the pano was found, compute the
     // position of the streetview image, then calculate the heading, then get a
     // panorama from that and set the options
@@ -106,57 +125,61 @@ function populateInfoWindow(marker, infowindow) {
           '<div>No Street View Found</div>');
       }
     }
+
     // Use streetview service to get the closest streetview image within
     // 50 meters of the markers position
     streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+
     // Open the infowindow on the correct marker.
     infowindow.open(map, marker);
   }
 }
 
-// This function will loop through the markers array and display them all.
-function showListings() {
-  var bounds = new google.maps.LatLngBounds();
-  // Extend the boundaries of the map for each marker and display the marker
-  for (var i = 0; i < markers.length; i++) {
-    markers[i].setMap(map);
-    bounds.extend(markers[i].position);
-  }
-  map.fitBounds(bounds);
+// Loops through the markers and filters them based on user search
+function filterMarkers() {
+  var searchText = document.querySelector('#filter-text').value.toLowerCase();
+  var filtered = markers.filter(function(marker) {
+    return marker.title.toLowerCase().includes(searchText);
+  });
+
+  clearMarkers();
+  filtered.forEach(function(marker) {
+    marker.setMap(map);
+  });
 }
 
-// This function will loop through the listings and hide them all.
-function hideListings() {
-  for (var i = 0; i < markers.length; i++) {
-    markers[i].setMap(null);
-  }
+// Clear all markers from the map
+function clearMarkers() {
+  markers.forEach(function(marker) {
+    marker.setMap(null);
+  });
 }
 
-// This function takes the input value in the find nearby area text input
-// locates it, and then zooms into that area. This is so that the user can
-// show all listings, then decide to focus on one area of the map.
-function zoomToArea() {
-  // Initialize the geocoder.
-  var geocoder = new google.maps.Geocoder();
-  // Get the address or place that the user entered.
-  var address = document.getElementById('zoom-to-area-text').value;
-  // Make sure the address isn't blank.
-  if (address == '') {
-    window.alert('You must enter an area, or address.');
-  } else {
-    // Geocode the address/area entered to get the center. Then, center the map
-    // on it and zoom in
-    geocoder.geocode(
-      { address: address,
-        componentRestrictions: {locality: 'New York'}
-      }, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          map.setCenter(results[0].geometry.location);
-          map.setZoom(15);
-        } else {
-          window.alert('We could not find that location - try entering a more' +
-              ' specific place.');
-        }
-      });
+// Begin app by initializing knockout and google map
+ko.applyBindings(new ViewModel());
+initMap();
+
+// Event handlers
+$('#filter-text').keyup(filterMarkers);
+$('#filter-button').click(filterMarkers);
+
+// https://api.foursquare.com/v2/venues/${foursquareId}?v=20171101&client_id=${clientId}&client_secret=${clientSecret}
+
+var foursquareId = '4aad43e3f964a5205a5f20e3';
+var clientId = '114EPTUAI3MLHVF24X4M2I3E5CUFL0UFONDKFCDOB410OPEI';
+var clientSecret = 'RRNXSXF4Q5HOJQXKLTIAHYP25KPL3A5TBIHDSIGEXYXVK3GM';
+
+$.ajax({
+
+  url: `https://api.yelp.com/v3/businesses/${foursquareId}`,
+  type: 'GET',
+  headers: {'Authorization': 'Bearer Mb84WblpHpm47Sa2Vwh5fqS6GlrYz69ZTKEhxrtOuQRdNwLGFgRen1Fa4pq8hS5tr9GKTpLSwns9ONYqzpqnnxNAmZ4MIhlbG-gBA7EI-j50FmzKTJP08timIr4EWnYx'},
+  jsonpCallback: 'cb',
+  cache: true,
+  success: function(data) {
+    console.log('SUCCESS');
   }
-}
+$.getJSON('https://api.foursquare.com/v2/venues/search?v=20171101&ll=41.878114%2C%20-87.629798&query=coffee&intent=checkin&client_id=114EPTUAI3MLHVF24X4M2I3E5CUFL0UFONDKFCDOB410OPEI&client_secret=RRNXSXF4Q5HOJQXKLTIAHYP25KPL3A5TBIHDSIGEXYXVK3GM').done((data) => console.log(data));
+  // Fetch the stored token from localStorage and set in the header
+  // headers: {"Authorization": localStorage.getItem('token')}
+});
